f249c845a8b06b485627ba81f55a9670c7a9cada
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2022-12-08 00:24:49 +0100

    Callexp

--- src/ast/AstVisitor.java
+++ src/ast/AstVisitor.java
@@ -19,6 +19,7 @@ public interface AstVisitor<T> {
 
 
  
+    public T visit(CallExp callExp);
     public T visit(IntLit intLit);
     public T visit(StringLit str);
     public T visit(Print print);
