6fdec28a26596a349488077f3f7e1eb7e3957596
SERRAND Coralie <coralie.serrand@telecomnancy.eu>
2022-12-28 16:40:23 +0100

    [-] Modif param

--- src/ast/Param.java
+++ src/ast/Param.java
@@ -1,13 +1,9 @@
 package ast;
 
-public class Param {
-
-    public Ast id;
-    public Ast type;
+public class Param extends Field{
 
     public Param(Ast id, Ast type) {
-        this.id = id;
-        this.type = type;
+        super(id, type);
     }
     
 }
